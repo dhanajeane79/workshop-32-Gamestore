940a70b4b4c20f43e354282117af879e01c1e940
dhanajeane79 <dhanajeane79@gmail.com>
2023-08-27 08:16:47 -0400

    Debugged an asynchronous function and SQL query to enable the endpoint.

--- api/videoGames.js
+++ api/videoGames.js
@@ -1,48 +1,52 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const REPLACE_ME = 'HELP REPLACE ME!!!!';
-
-const { getAllVideoGames,
-    getVideoGameById,
-    createVideoGame,
-    updateVideoGame,
-    deleteVideoGame } = require('../db/videoGames');
+const {
+  getAllVideoGames,
+  getVideoGameById,
+  createVideoGame,
+  updateVideoGame,
+  deleteVideoGame,
+} = require("../db/videoGames");
 
 // GET - /api/video-games - get all video games
-router.get('/', async (req, res, next) => {
-    try {
-        const videoGames = await getAllVideoGames();
-        res.send(videoGames);
-    } catch (error) {
-        next(error);
-    }
+// Define the route handler for GET /api/video-games
+// Call the function to retrieve all video games
+// Send the retrieved video games as a response
+// Handle errors by passing them to the next middleware
+router.get("/", async (req, res, next) => {
+  try {
+    const videoGames = await getAllVideoGames();
+
+    res.send(videoGames);
+  } catch (error) {
+    next(error);
+  }
 });
 
 // GET - /api/video-games/:id - get a single video game by id
-router.get('/:id', async (req, res, next) => {
-    try {
-        const videoGame = await getVideoGameById(REPLACE_ME);
-        res.send(videoGame);
-    } catch (error) {
-        next(error);
-    }
+router.get("/:id", async (req, res, next) => {
+  try {
+    const videoGame = await getVideoGameById(REPLACE_ME);
+    res.send(videoGame);
+  } catch (error) {
+    next(error);
+  }
 });
 
 // POST - /api/video-games - create a new video game
-router.patch('/', async (req, res, next) => {
-    // LOGIC GOES HERE 
+router.patch("/", async (req, res, next) => {
+  // LOGIC GOES HERE
 });
 
-
 // PUT - /api/video-games/:id - update a single video game by id
-router.put('/:id', async (req, res, next) => {
-    // LOGIC GOES HERE 
+router.put("/:id", async (req, res, next) => {
+  // LOGIC GOES HERE
 });
 
 // DELETE - /api/video-games/:id - delete a single video game by id
-router.delete('/:id', async (req, res, next) => {
-    // LOGIC GOES HERE
+router.delete("/:id", async (req, res, next) => {
+  // LOGIC GOES HERE
 });
 
 module.exports = router;
